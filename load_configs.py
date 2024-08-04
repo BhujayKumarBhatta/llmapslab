73976cf02a658df3e137bbda1905e712818146a9
Bhujay Kumar Bhatta <bhujay.bhatta@yahoo.com>
2024-08-04 20:00:26 +0530

    promptTemplate learning

--- load_configs.py
+++ load_configs.py
@@ -6,3 +6,6 @@ import json
 with open('../secrets.json', 'r') as jsonfile:
     configs = json.load(jsonfile)
 openai_api_key=configs.get('openai_api_key')
+llama_api_key = configs.get('llama_api_key')
+ai21_api_key = configs.get('ai21_api_key')
+gemini_api_key = configs.get('gemini_api_key')
