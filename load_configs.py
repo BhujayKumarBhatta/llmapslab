ec2b72e178579faeb2bdcc5c0123c07fd563b82e
Bhujay Kumar Bhatta <bhujay.bhatta@yahoo.com>
2024-08-18 17:15:35 +0530

    langchain_openai manages the secret

--- load_configs.py
+++ load_configs.py
@@ -3,9 +3,37 @@ sys.path.append("llmapslab")
 import os
 import json
 
+# with open('../secrets.json', 'r') as jsonfile:
+#     configs = json.load(jsonfile)
+# openai_api_key=configs.get('openai_api_key')
+# llama_api_key = configs.get('llama_api_key')
+# ai21_api_key = configs.get('ai21_api_key')
+# gemini_api_key = configs.get('gemini_api_key')
+
+from pydantic import BaseModel, SecretStr
+
+class ApiConfig(BaseModel):
+    openai_api_key: SecretStr
+    llama_api_key: SecretStr
+    ai21_api_key: SecretStr
+    gemini_api_key: SecretStr
+
+    def __str__(self):
+        return "ApiConfig(secrets hidden)"
+
+import json
+
 with open('../secrets.json', 'r') as jsonfile:
     configs = json.load(jsonfile)
-openai_api_key=configs.get('openai_api_key')
-llama_api_key = configs.get('llama_api_key')
-ai21_api_key = configs.get('ai21_api_key')
-gemini_api_key = configs.get('gemini_api_key')
+
+api_config = ApiConfig(
+    openai_api_key=configs.get('openai_api_key'),
+    llama_api_key=configs.get('llama_api_key'),
+    ai21_api_key=configs.get('ai21_api_key'),
+    gemini_api_key=configs.get('gemini_api_key')
+)
+
+openai_api_key=api_config.openai_api_key
+llama_api_key=api_config.llama_api_key
+ai21_api_key=api_config.ai21_api_key
+gemini_api_key=api_config.gemini_api_key
